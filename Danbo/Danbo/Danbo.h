f73473fe02cb85ab8e6b9e48b192fb26e7a133bf
史 翔新 <shi_xiangxin@mages.co.jp>
2017-05-10 17:56:47 +0900

    Add Playground to project

---
+++ Danbo/Danbo/Danbo.h
@@ -0,0 +1,19 @@
+//
+//  Danbo.h
+//  Danbo
+//
+//  Created by 史　翔新 on 2017/05/10.
+//  Copyright © 2017年 Crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Danbo.
+FOUNDATION_EXPORT double DanboVersionNumber;
+
+//! Project version string for Danbo.
+FOUNDATION_EXPORT const unsigned char DanboVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Danbo/PublicHeader.h>
+
+
