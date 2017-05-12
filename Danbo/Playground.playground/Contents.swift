dc73a7481fb7d091a76035ff1b8dc9cfbe7533b3
星野恵瑠 <shonetesmith@gmail.com>
2017-05-13 00:39:50 +0900

    Playground content implements minor update

--- Danbo/Playground.playground/Contents.swift
+++ Danbo/Playground.playground/Contents.swift
@@ -14,38 +14,25 @@ let imageView = UIImageView(image: image)
 imageView.center = CGPoint(x: 240, y: 480)
 view.addSubview(imageView)
 
-extension UIView {
-	
-	func rotateOnGround(by rotation: Rotation, stretching yScale: CGFloat) {
-		
-		let oldHeight = self.bounds.height
-		let newHeight = oldHeight * yScale
-		let translationX = newHeight * sin(rotation) / 2
-		let translationY = (oldHeight - (newHeight * cos(rotation))) / 2
-		
-		self.danbo.transform { $0
-			.reset()
-			.scaleBy(dy: yScale)
-			.rotate(by: rotation)
-			.translateBy(dx: translationX, dy: translationY)
-			.commit()
-		}
-		
-	}
-	
-}
 
 let rotation: Rotation = .degree(15)
 let yStretching: CGFloat = 1 / cos(rotation.radianValue)
 let yZoom: CGFloat = 1
 let duration: TimeInterval = 0.6
 
-func animate() {
+func rotate(_ view: UIView, onGroundBy rotation: Rotation, stretching yScale: CGFloat) {
 	
-	UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: { 
-		imageView.rotateOnGround(by: -rotation, stretching: yStretching)
-	}) { (_) in
-		loop()
+	let oldHeight = view.bounds.height
+	let newHeight = oldHeight * yScale
+	let translationX = newHeight * sin(rotation) / 2
+	let translationY = (oldHeight - (newHeight * cos(rotation))) / 2
+	
+	view.danbo.transform { $0
+		.reset()
+		.scaleBy(dy: yScale)
+		.rotate(by: rotation)
+		.translateBy(dx: translationX, dy: translationY)
+		.commit()
 	}
 	
 }
@@ -55,15 +42,26 @@ func loop() {
 	UIView.animateKeyframes(withDuration: 1, delay: 0, options: [.calculationModePaced, .autoreverse, .repeat], animations: {
 		
 		UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
-			imageView.rotateOnGround(by: 0, stretching: yZoom)
+			rotate(imageView, onGroundBy: 0, stretching: yZoom)
 		})
 		
 		UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
-			imageView.rotateOnGround(by: rotation, stretching: yStretching)
+			rotate(imageView, onGroundBy: rotation, stretching: yStretching)
 		})
 		
 	}, completion: nil)
 	
 }
+
+func animate() {
+	
+	UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+		rotate(imageView, onGroundBy: -rotation, stretching: yStretching)
+	}) { (_) in
+		loop()
+	}
+	
+}
+
 
 animate()
