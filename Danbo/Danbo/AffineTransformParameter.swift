39a40e2a3925ca2912d87c900ebdfb1e875c773b
星野恵瑠 <shonetesmith@gmail.com>
2017-05-12 21:40:36 +0900

    Add a UIView wrapper Danbo for transform operations

---
+++ Danbo/Danbo/AffineTransformParameter.swift
@@ -0,0 +1,16 @@
+//
+//  AffineTransformParameter.swift
+//  Danbo
+//
+//  Created by 史翔新 on 2017/05/12.
+//  Copyright © 2017年 Crazism. All rights reserved.
+//
+
+import Foundation
+
+enum AffineTransformParameter {
+	
+	case to(CGAffineTransform)
+	case by(CGAffineTransform)
+	
+}
